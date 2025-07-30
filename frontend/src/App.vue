20767791f21a856d70e10d8f9ddb0bb209862d99
valentinzamarin <hello.zamarin@gmail.com>
2025-07-30 03:42:54 +0300

    app init

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup type="module">
+import { ref, onMounted } from 'vue';
+
+const message = ref('Загрузка...');
+
+onMounted(async () => {
+  try {
+    const response = await fetch('http://localhost/api/hello');
+
+    if (!response.ok) {
+      throw new Error(`Ошибка HTTP: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    message.value = data.message;
+    console.log(message.value);
+
+  } catch (error) {
+    console.error('Ошибка при получении данных:', error);
+    message.value = 'Не удалось загрузить сообщение.';
+  }
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    {{ message }}
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
